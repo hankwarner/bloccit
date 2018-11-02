113d06e816b86e66dde6f5992d06ee311a5d9e1c
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-11-01 20:49:27 -0400

    still working through assignment

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     init(app){
-        const staticRoutes = require('../routes/static');
-        app.use(staticRoutes);
+      const staticRoutes = require("../routes/static");
+      app.use(staticRoutes);
     }
-}
+  }
