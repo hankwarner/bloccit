277d06494f41302b57aae858bf1c3e516462daed
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-11-01 20:34:38 -0400

    checking with support

--- src/routes/static.js
+++ src/routes/static.js
@@ -4,3 +4,7 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
     res.send('Welcome to Bloccit')
 });
+
+router.get("/marco", (req, res, next) => {
+    res.send('polo')
+});
