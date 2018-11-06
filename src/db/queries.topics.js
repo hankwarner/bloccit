29af291b96923a9c01989472fb6e22a6982006ec
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-11-06 17:59:15 -0500

    working on checkpoint tests

---
+++ src/db/queries.topics.js
@@ -0,0 +1,14 @@
+const Topic = require('./models').Topics;
+
+module.exports = {
+    getAllTopics(callback) {
+        return Topics.all()
+
+        .then((topics) => {
+            callback(null, topics);
+        })
+        .catch((err) => {
+            callback(err);
+        })
+    }
+}
