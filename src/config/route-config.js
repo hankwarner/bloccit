3f10189b35a82a249e2a18b042225067edfb2187
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-11-17 12:45:01 -0500

    authentication: checkpoint completed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,10 +4,12 @@ module.exports = {
         const topicRoutes = require("../routes/topics");
         const postRoutes = require("../routes/posts");
         const tagRoutes = require("../routes/tags");
+        const userRoutes = require("../routes/users");
 
         app.use(staticRoutes);
         app.use(topicRoutes);
         app.use(postRoutes);
         app.use(tagRoutes);
+        app.use(userRoutes);
     }
 }
