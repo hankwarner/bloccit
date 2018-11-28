a4c68600cf239af2d72cbd029b95aa8e6e4839b3
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-11-27 23:15:00 -0500

    favoriting: checkpoint completed

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -44,12 +44,23 @@ module.exports = (sequelize, DataTypes) => {
       as: "favorites"
     });
 
+    Post.afterCreate((post, callback) => {
+      return models.Favorite.create({
+        userId: post.userId,
+        postId: post.id
+      });
+    });
+
     Post.prototype.getPoints = function(){
       if(this.votes.length === 0) return 0
       return this.votes
         .map((v) => { return v.value })
         .reduce((prev, next) => { return prev + next });
-      }
+    }
+
+    Post.prototype.getFavoriteFor = function(userId){
+      return this.favorites.find((favorite) => { return favorite.userId == userId });
+    }
   }
   return Post;
 }
